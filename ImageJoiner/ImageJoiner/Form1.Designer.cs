2479b811c887a880cf214f579181f6ba2f44e5fe
kblazelek <traxxstudios@gmail.com>
2016-02-26 00:41:24 +0100

    Added dropdown listview with image icons and test images

--- ImageJoiner/ImageJoiner/Form1.Designer.cs
+++ ImageJoiner/ImageJoiner/Form1.Designer.cs
@@ -29,11 +29,60 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listViewImages = new System.Windows.Forms.ListView();
+            this.imagesList = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listViewImages
+            // 
+            this.listViewImages.AllowDrop = true;
+            this.listViewImages.Location = new System.Drawing.Point(12, 12);
+            this.listViewImages.Name = "listViewImages";
+            this.listViewImages.Size = new System.Drawing.Size(228, 361);
+            this.listViewImages.SmallImageList = this.imagesList;
+            this.listViewImages.TabIndex = 0;
+            this.listViewImages.UseCompatibleStateImageBehavior = false;
+            this.listViewImages.View = System.Windows.Forms.View.List;
+            this.listViewImages.DragDrop += new System.Windows.Forms.DragEventHandler(this.listViewImages_DragDrop);
+            this.listViewImages.DragEnter += new System.Windows.Forms.DragEventHandler(this.listViewImages_DragEnter);
+            // 
+            // imagesList
+            // 
+            this.imagesList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imagesList.ImageStream")));
+            this.imagesList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imagesList.Images.SetKeyName(0, "asd.bmp");
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(247, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(525, 361);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listViewImages);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.Text = "ImageJoiner";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listViewImages;
+        private System.Windows.Forms.ImageList imagesList;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- ImageJoiner/ImageJoiner/Form1.cs
+++ ImageJoiner/ImageJoiner/Form1.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,67 @@ namespace ImageJoiner
 {
     public partial class Form1 : Form
     {
+        protected bool validData;
+        int currentListIndex = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void listViewImages_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if(files.Length != 0)
+                {
+                    validData = true;
+                }
+                else
+                {
+                    validData = false;
+                }
+                foreach (string file in files)
+                {
+                    FileInfo fi = new FileInfo(file);
+                    if (fi.Extension != ".jpg" && fi.Extension != ".png" && fi.Extension != ".bmp")
+                    {
+                        validData = false;
+                    }
+
+                    if (validData)
+                    {
+                        e.Effect = DragDropEffects.Copy;
+                    }
+                    else
+                        e.Effect = DragDropEffects.None;
+                }
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void listViewImages_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                if (validData)
+                {
+                    string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                    foreach (string file in files)
+                    {
+                        FileInfo fi = new FileInfo(file);
+                        imagesList.Images.Add(Image.FromFile(file));
+                        ListViewItem item = new ListViewItem();
+                        item.Text = file;
+                        item.ImageIndex = currentListIndex;
+                        ++currentListIndex;
+                        listViewImages.Items.Add(item);
+                    }
+                }
+            }
         }
     }
 }
