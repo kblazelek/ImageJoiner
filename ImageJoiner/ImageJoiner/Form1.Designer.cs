f4d2093719b7635f6296b9f40f622cb65aa9761c
kblazelek <traxxstudios@gmail.com>
2016-02-28 01:34:46 +0100

    Added horizontal scroll to listview, drawing joined image and panning

--- ImageJoiner/ImageJoiner/Form1.Designer.cs
+++ ImageJoiner/ImageJoiner/Form1.Designer.cs
@@ -32,20 +32,25 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.listViewImages = new System.Windows.Forms.ListView();
             this.imagesList = new System.Windows.Forms.ImageList(this.components);
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pictureBoxFinallImage = new System.Windows.Forms.PictureBox();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonJoinImages = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFinallImage)).BeginInit();
             this.SuspendLayout();
             // 
             // listViewImages
             // 
             this.listViewImages.AllowDrop = true;
-            this.listViewImages.Location = new System.Drawing.Point(12, 12);
+            this.listViewImages.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listViewImages.Location = new System.Drawing.Point(13, 12);
             this.listViewImages.Name = "listViewImages";
             this.listViewImages.Size = new System.Drawing.Size(228, 361);
             this.listViewImages.SmallImageList = this.imagesList;
             this.listViewImages.TabIndex = 0;
             this.listViewImages.UseCompatibleStateImageBehavior = false;
-            this.listViewImages.View = System.Windows.Forms.View.List;
+            this.listViewImages.View = System.Windows.Forms.View.SmallIcon;
             this.listViewImages.DragDrop += new System.Windows.Forms.DragEventHandler(this.listViewImages_DragDrop);
             this.listViewImages.DragEnter += new System.Windows.Forms.DragEventHandler(this.listViewImages_DragEnter);
             // 
@@ -55,25 +60,52 @@
             this.imagesList.TransparentColor = System.Drawing.Color.Transparent;
             this.imagesList.Images.SetKeyName(0, "asd.bmp");
             // 
-            // pictureBox1
+            // pictureBoxFinallImage
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(247, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(525, 361);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.pictureBoxFinallImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxFinallImage.Location = new System.Drawing.Point(247, 12);
+            this.pictureBoxFinallImage.Name = "pictureBoxFinallImage";
+            this.pictureBoxFinallImage.Size = new System.Drawing.Size(525, 537);
+            this.pictureBoxFinallImage.TabIndex = 1;
+            this.pictureBoxFinallImage.TabStop = false;
+            this.pictureBoxFinallImage.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxFinallImage_Paint);
+            this.pictureBoxFinallImage.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxFinallImage_MouseDown);
+            this.pictureBoxFinallImage.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBoxFinallImage_MouseMove);
+            this.pictureBoxFinallImage.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBoxFinallImage_MouseUp);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Width = -1;
+            // 
+            // buttonJoinImages
+            // 
+            this.buttonJoinImages.Location = new System.Drawing.Point(13, 388);
+            this.buttonJoinImages.Name = "buttonJoinImages";
+            this.buttonJoinImages.Size = new System.Drawing.Size(228, 23);
+            this.buttonJoinImages.TabIndex = 2;
+            this.buttonJoinImages.Text = "Join images";
+            this.buttonJoinImages.UseVisualStyleBackColor = true;
+            this.buttonJoinImages.Click += new System.EventHandler(this.buttonJoinImages_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonJoinImages);
+            this.Controls.Add(this.pictureBoxFinallImage);
             this.Controls.Add(this.listViewImages);
             this.MinimumSize = new System.Drawing.Size(800, 600);
             this.Name = "Form1";
             this.Text = "ImageJoiner";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFinallImage)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -82,7 +114,10 @@
 
         private System.Windows.Forms.ListView listViewImages;
         private System.Windows.Forms.ImageList imagesList;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBoxFinallImage;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Button buttonJoinImages;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
     }
 }
 
--- ImageJoiner/ImageJoiner/Form1.cs
+++ ImageJoiner/ImageJoiner/Form1.cs
@@ -16,6 +16,11 @@ namespace ImageJoiner
     {
         protected bool validData;
         int currentListIndex = 0;
+        Point startingPoint = Point.Empty;
+        Bitmap finalImage;
+        Point finallImagePosition = Point.Empty;
+        Point movingPoint = Point.Empty;
+        bool panning = false;
         public Form1()
         {
             InitializeComponent();
@@ -26,7 +31,7 @@ namespace ImageJoiner
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
                 string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
-                if(files.Length != 0)
+                if (files.Length != 0)
                 {
                     validData = true;
                 }
@@ -74,6 +79,122 @@ namespace ImageJoiner
                         listViewImages.Items.Add(item);
                     }
                 }
+            }
+        }
+
+        private void buttonJoinImages_Click(object sender, EventArgs e)
+        {
+            int imagesCount = listViewImages.Items.Count;
+            string[] files;
+            if (imagesCount > 0)
+            {
+                files = new string[imagesCount];
+                files = listViewImages.Items.Cast<ListViewItem>().Select(item => item.Text).ToArray();
+                Array.Sort(files);
+                pictureBoxFinallImage.Image = joinImages(files); // dodac sprawdzanie czy files wypelnione
+            }
+            else
+            {
+                MessageBox.Show("There are no images to join.");
+            }
+        }
+        // Dodać sprawdzanie nazw i rozmiarów obrazu
+        private Bitmap joinImages(string[] files)
+        {
+            // Images contain images, rectangles contain image positions
+            List<Tuple<Image, Rectangle>> imagesWithRectangles = new List<Tuple<Image, Rectangle>>();
+            string fileNameWithoutExtension;
+            int row, column;
+            int xPosition = 0;
+            int yPosition = 0;
+
+            try
+            {
+                int width = 0;
+                int height = 0;
+
+                foreach (string image in files)
+                {
+                    fileNameWithoutExtension = Path.GetFileNameWithoutExtension(image);
+                    row = Int32.Parse(fileNameWithoutExtension.Split('_')[0]);
+                    column = Int32.Parse(fileNameWithoutExtension.Split('_')[1]);
+                    //create a Bitmap from the file and add it to the list
+                    Bitmap bitmap = new Bitmap(image);
+
+                    //update the size of the final bitmap
+                    xPosition = column * bitmap.Width;
+                    yPosition = row * bitmap.Height;
+                    if (xPosition + bitmap.Width > width) width = xPosition + bitmap.Width;
+                    if (yPosition + bitmap.Height > height) height = yPosition + bitmap.Height;
+                    Tuple<Image, Rectangle> imageWithRectangle = new Tuple<Image, Rectangle>(bitmap, new Rectangle(xPosition, yPosition, bitmap.Width, bitmap.Height));
+                    imagesWithRectangles.Add(imageWithRectangle);
+                }
+
+                //create a bitmap to hold the combined image
+                finalImage = new Bitmap(width, height);
+
+                //get a graphics object from the image so we can draw on it
+                using (Graphics g = Graphics.FromImage(finalImage))
+                {
+                    //set background color
+                    g.Clear(Color.Black);
+
+                    //go through each image and draw it on the final image
+                    foreach (var imageWithRectangle in imagesWithRectangles)
+                    {
+                        g.DrawImage(imageWithRectangle.Item1, imageWithRectangle.Item2);
+                    }
+                }
+
+                return finalImage;
+            }
+            catch (Exception ex)
+            {
+                if (finalImage != null)
+                    finalImage.Dispose();
+
+                throw ex;
+            }
+            finally
+            {
+                //clean up memory
+                for (int i = 0; i < imagesWithRectangles.Count; ++i)
+                {
+                    var tempTuple = imagesWithRectangles.ElementAt(i);
+                    tempTuple.Item1.Dispose();
+                    tempTuple = null;
+                }
+            }
+        }
+
+        private void pictureBoxFinallImage_MouseDown(object sender, MouseEventArgs e)
+        {
+            panning = true;
+            startingPoint = e.Location;
+        }
+
+        private void pictureBoxFinallImage_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (panning && (finalImage != null))
+            {
+                movingPoint = new Point(finallImagePosition.X + e.Location.X - startingPoint.X,
+                                        finallImagePosition.Y + e.Location.Y - startingPoint.Y);
+                finallImagePosition = new Point(finallImagePosition.X + e.Location.X - startingPoint.X, finallImagePosition.Y + e.Location.Y - startingPoint.Y);
+                pictureBoxFinallImage.Invalidate();
+            }
+        }
+
+        private void pictureBoxFinallImage_MouseUp(object sender, MouseEventArgs e)
+        {
+            panning = false;
+        }
+
+        private void pictureBoxFinallImage_Paint(object sender, PaintEventArgs e)
+        {
+            if (finalImage != null)
+            {
+                e.Graphics.Clear(Color.Black);
+                e.Graphics.DrawImage(finalImage, movingPoint);
             }
         }
     }
